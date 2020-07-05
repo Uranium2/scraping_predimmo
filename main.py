623bc08bbc61e7c756c3b7981f0db103eeb29fe4
Arastal <bergeron.florian@gmail.com>
2020-07-05 18:08:23 +0200

    [UP] - Add DocString & Refactor, don't hesitate to check refactor.

--- main.py
+++ main.py
@@ -2,3 +2,4 @@ from scrap import run_scrapping
 
 if __name__ == "__main__":
     run_scrapping()
+    print("\n*** SCRAPPING STATUS: STOP ***\n")
--- scrap.py
+++ scrap.py
@@ -1,16 +1,16 @@
-import pandas as pd
-from bs4 import BeautifulSoup
-import requests
-from csv import writer
 import os
-from datetime import datetime
+import json
 import time
 import random
 import pymysql
-import json
+import requests
+import pandas as pd
+
+from csv import writer
+from bs4 import BeautifulSoup
+from datetime import datetime
 from fake_headers import Headers
 
-# !pip install fake-useragent
 
 url_ref = "https://www.leboncoin.fr"
 url = "https://www.leboncoin.fr/_immobilier_/offres/ile_de_france/"
@@ -66,21 +66,39 @@ url_page = "https://www.leboncoin.fr/_immobilier_/offres/ile_de_france/p-{}/"
 
 
 def append_list_as_row(file_path, l):
+    """ Add a list as row in a "filepath".csv
+
+    Args:
+        file_path [Stringtype]: Set the filepath.
+        l [List]: Set the list to append to the "filepath".csv.
+    """
     script_dir = os.path.dirname(__file__)
     abs_file_path = os.path.join(script_dir, file_path)
+    
     with open(abs_file_path, 'a+', newline='', encoding='utf-8') as write_obj:
         csv_writer = writer(write_obj)
         csv_writer.writerow(l)
 
+
 def scrap_page(url, date):
+    """ Scrap a specific URL with specific date.
+
+    Args:
+        url [String]: Get the scrap page URL.
+        date [Int]: Get the scrap date.
+
+    Write [.csv]: Write scrapping in CSV.
+    """
     response = requests.get(url, headers=header.generate())
     print(url)
     if (response.status_code != 200):
         print(response.status_code)
         return
+        
     soup = BeautifulSoup(response.text, 'html.parser')
 
     list_immo = []
+
     for a in soup.find_all('a', href=True):
         list_immo.append(a['href'])
 
@@ -94,7 +112,8 @@ def scrap_page(url, date):
 
     for elm in list_immo:
         time.sleep(5)
-        # store all info inside values_col and append to file
+
+        # Store all info inside values_col and append to file
         values_col = []
         values_col.append(date)
 
@@ -110,24 +129,20 @@ def scrap_page(url, date):
         description = soup_immo.find("span", class_="_1fFkI").text
         description = description.replace(',', ' ')
         description = description.replace('\n', ' ')
-        # print(description)
+        
         # Price
         try:
             price = int(soup_immo.find("span", class_="_3Ce01 _3gP8T _25LNb _35DXM").text.replace("€", "").replace(" ", ""))
-            # print(price)
         except:
             print(build_url + " => No price")
             continue
 
         
-        # Localisation city + zip code
+        # City & zip code location
         localisation_all = soup_immo.find_all("h2", class_="Roh2X _3c6yv _25dUs _21rqc _3QJkO _1hnil _1-TTU _35DXM")[2]
 
         city = localisation_all.text.split("(")[0]
-        # print(city)
         zip_code = localisation_all.text.split("(")[-1].split(")")[0]
-        # print(zip_code)
-
 
         values_col.append(build_url)
         values_col.append(description)
@@ -153,13 +168,14 @@ def scrap_page(url, date):
             if (div.text == "Pièces"):
                 values_col.append(div.findNext("p").text)
 
-        # Writte to csv
+        # Write to csv
         send_to_rds(values_col, conn)
         append_list_as_row("data/{}.csv".format(date), values_col)
 
 
-
 def run_scrapping():
+    """ Start scrapping on specific URL page and date.
+    """
     date = datetime.now().strftime("%Y-%m-%d")
     size = 100
     r = list(range(size))
@@ -170,16 +186,29 @@ def run_scrapping():
 
 
 def create_conn():
+    """ Create connection to RDS wyth PyMySQL. 
+
+    Returns:
+        pymysql [String]: SQL Credentials.
+    """
     dir_path = os.path.dirname(os.path.realpath(__file__))
     f = open(os.path.join(dir_path, "aws_keys"), "r")
     keys = f.read().split("\n")
+
     return pymysql.connect(
         host= keys[2],
         user=keys[3],
         password=keys[4],
         port=int(keys[5]))
 
+
 def send_to_rds(data, conn):
+    """ Upload a dataframe in RDS by PyMySQL.
+
+    Args:
+        data [Dataframe]: Get a dataframe to upload.
+        conn [String]: SQL Statement to RDS.
+    """
     if (len(data) < 9):
         return
     cursor = conn.cursor()
@@ -205,17 +234,29 @@ def send_to_rds(data, conn):
 
 
 def get_coord_from_address(code_postal, adresse=None):
+    """ Get longitude and latitude from an address.
+
+    Args:
+        code_postal [String]: Get zip code.
+        adresse [String] (optional): Get address (Defaults to None).
+
+    Returns:
+        pos [List]: This is a list of longitude and latitude.
+    """
     headers = {"Content-Type": "application/json"}
     if adresse != None:
         url = str(("http://api-adresse.data.gouv.fr/search/?q=" + str(adresse) + "&postcode=" + str(code_postal)))
     else:
         url = str(("http://api-adresse.data.gouv.fr/search/?q=" + str(code_postal)))
+
     r = requests.get(url, headers=header.generate(), data="")
     js = json.loads(r.text)
     x = js['features'][0]['geometry']['coordinates']
     longitude = x[0]
     latitude = x[1]
+    
     pos = []
     pos.append(longitude)
     pos.append(latitude)
+
     return pos
